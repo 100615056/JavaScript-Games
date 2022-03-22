d7ed82c8a7b7db1471c972fd30f9d39c8cbada0a
Angelica <gelimhc2@hotmail.com>
2022-03-21 20:21:40 -0400

    add bullets to player

--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,89 @@
 export default class Player {
-    constructor(x, y) {
+    constructor(x, y, bulletController) {
         this.x = x;
         this.y = y;
-        this.width = 50;
-        this.height = 50;
+        this.bulletController = bulletController;
+        this.radius = 40;
+        this.startAngle = 0;
+        this.endAngle = Math.PI * 2;
         this.speed = 4;
+
+        document.addEventListener('keydown', this.keydown);
+        document.addEventListener('keyup', this.keyup);
     }
     
     draw(ctx) {
-        ctx.strokeStyle = "yellow";
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.move();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+        ctx.fillStyle = "#FF8784";
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#F9F871";
+        ctx.stroke();
+        this.shoot();
     }
+
+    shoot() {
+        if (this.shootPressed) {
+            console.log('Shoot')
+            const speed = 5;
+            const delay = 7;
+            const damage = 1;
+            const bulletX = this.x + this.radius / 30;
+            const bulletY = this.y;
+            this.bulletController.shoot(bulletX, bulletY, speed, damage, delay);
+        }
+    }
+
+    move() {
+        if (this.downPressed) {
+            this.y += this.speed;
+        }
+        if (this.upPressed) {
+            this.y -= this.speed;
+        }
+        if (this.leftPressed) {
+            this.x -= this.speed;
+        }
+        if (this.rightPressed) {
+            this.x += this.speed;
+        }
+    }
+
+    keydown = (e) => {
+        if (e.code === "ArrowUp") {
+            this.upPressed = true;
+        }
+        if (e.code === "ArrowDown") {
+            this.downPressed = true;
+        }
+        if (e.code === "ArrowLeft") {
+            this.leftPressed = true;
+        }
+        if (e.code === "ArrowRight") {
+            this.rightPressed = true;
+        }
+        if (e.code === "Space") {
+            this.shootPressed = true;
+        }
+    };
+
+    keyup = (e) => {
+        if (e.code === "ArrowUp") {
+            this.upPressed = false;
+        }
+        if (e.code === "ArrowDown") {
+            this.downPressed = false;
+        }
+        if (e.code === "ArrowLeft") {
+            this.leftPressed = false;
+        }
+        if (e.code === "ArrowRight") {
+            this.rightPressed = false;
+        }
+        if (e.code === "Space") {
+            this.shootPressed = false;
+        }
+    };
 }
--- src/bubble.js
+++ src/bubble.js
@@ -1,4 +1,5 @@
 import Player from "./Player.js";
+import BulletController from "./BulletController.js";
 
 // Reference canvas and get context
 const canvas = document.getElementById('gameArea');
@@ -8,18 +9,27 @@ const ctx = canvas.getContext('2d');
 canvas.width = 550;
 canvas.height = 600;
 
+const bulletController = new BulletController(canvas);
+
 const player = new Player(
     canvas.width / 2.2,
-    canvas.height / 1.3
+    canvas.height / 1.3,
+    bulletController
 );
 
 function gameLoop () {
+    setCommonStyle();
     clearScreen();
+    bulletController.draw(ctx);
     player.draw(ctx);
 }
 
+function setCommonStyle() {
+    ctx.shadowColor = "#F9F871";
+    ctx.shadowBlur = 20;
+}
 function clearScreen() {
-    ctx.fillStyle = "skyblue";
+    ctx.fillStyle = "#87CEFA";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
